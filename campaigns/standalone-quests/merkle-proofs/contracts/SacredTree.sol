cae443fc9dc0a9eb99997edabccc5b723a32a7b6
박진환 <parkjinhwan@bagjinhwan-ui-MacBookPro.local>
2025-02-24 09:35:18 +0900

    Download quest merkle-proofs

---
+++ campaigns/standalone-quests/merkle-proofs/contracts/SacredTree.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SacredTree {
+
+    bytes32 public root; // PART 1: STORE ROOT HERE
+
+    /// Verify that an address is trusted by the tree.
+    /// @param trustee Address to verify
+    /// @param proof Merkle proof for verification
+    function verify(
+        address trustee, 
+        bytes32[] calldata proof) 
+    external view returns (bool) {
+        // PART 2: CODE HERE
+    }
+
+}
